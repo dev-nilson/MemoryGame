5f20fc421743bea7c0a86e69e76474f35904c36b
Denilson Lemus <dlemus@harding.edu>
2022-03-15 13:18:54 -0500

    Set initial project

---
+++ Memory/app/src/main/java/com/devnilson/memory/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.devnilson.memory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.GridLayout
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var game: MemoryGame
+    private lateinit var memoryGridLayout: GridLayout
+    private var selectedLightColor = 0
+    private var unselectedLightColor = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        memoryGridLayout = findViewById(R.id.memory_grid)
+        selectedLightColor = ContextCompat.getColor(this, R.color.blue)
+        unselectedLightColor = ContextCompat.getColor(this, R.color.gray)
+
+        game = MemoryGame()
+        startGame()
+    }
+
+    private fun startGame() {
+        game.newGame()
+        setButtonColors()
+    }
+
+    private fun setButtonColors() {
+        for (buttonIndex in 0 until memoryGridLayout.childCount) {
+            val gridButton = memoryGridLayout.getChildAt(buttonIndex)
+
+            val row = buttonIndex / GRID_SIZE
+            val col = buttonIndex % GRID_SIZE
+
+            if (game.isLightSelected(row, col)) {
+                gridButton.setBackgroundColor(selectedLightColor)
+            } else {
+                gridButton.setBackgroundColor(unselectedLightColor)
+            }
+        }
+    }
+}
---
+++ Memory/app/src/main/java/com/devnilson/memory/MemoryGame.kt
@@ -0,0 +1,20 @@
+package com.devnilson.memory
+import kotlin.random.Random
+
+const val GRID_SIZE = 4
+
+class MemoryGame {
+    private val lightsGrid = Array(GRID_SIZE) { Array(GRID_SIZE) { true } }
+
+    fun newGame() {
+        for (row in 0 until GRID_SIZE) {
+            for (col in 0 until GRID_SIZE) {
+                lightsGrid[row][col] = Random.nextBoolean()
+            }
+        }
+    }
+
+    fun isLightSelected(row: Int, col: Int): Boolean {
+        return lightsGrid[row][col]
+    }
+}
